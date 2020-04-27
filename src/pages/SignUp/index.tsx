faa3495384d69aa78da20d0127ed510b05bdb3c4
Alexandre Pellegrino <alesirius@gmail.com>
2020-04-27 03:12:32 -0300

    updates

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,37 +3,43 @@ import React from 'react';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
+import { Form } from '@unform/web';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-    <Container>
-        <Background />
-        <Content>
-            <img src={logoImg} alt="GoBarber" />
+const SignUp: React.FC = () => {
+    function handleSubmit(data: object): void {
+        console.log(data);
+    }
+    return (
+        <Container>
+            <Background />
+            <Content>
+                <img src={logoImg} alt="GoBarber" />
 
-            <form>
-                <h1>Registre-se</h1>
+                <Form onSubmit={handleSubmit}>
+                    <h1>Registre-se</h1>
 
-                <Input icon={FiUser} name="name" placeholder="Nome" />
+                    <Input icon={FiUser} name="name" placeholder="Nome" />
 
-                <Input icon={FiMail} name="email" placeholder="E-mail" />
+                    <Input icon={FiMail} name="email" placeholder="E-mail" />
 
-                <Input
-                    icon={FiLock}
-                    name="password"
-                    type="password"
-                    placeholder="Senha"
-                />
+                    <Input
+                        icon={FiLock}
+                        name="password"
+                        type="password"
+                        placeholder="Senha"
+                    />
 
-                <Button type="submit">Cadastrar</Button>
-            </form>
-            <a href="teste">
-                <FiArrowLeft />
-                Voltar
-            </a>
-        </Content>
-    </Container>
-);
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+                <a href="teste">
+                    <FiArrowLeft />
+                    Voltar
+                </a>
+            </Content>
+        </Container>
+    );
+};
 
 export default SignUp;

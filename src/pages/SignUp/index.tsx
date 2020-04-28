2d517021910eaee05318f3b69570b7c2ec67b81f
Alexandre Pellegrino <alesirius@gmail.com>
2020-04-27 23:31:24 -0300

    yup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-    function handleSubmit(data: object): void {
-        console.log(data);
-    }
+
+    const formRef = useRef(null);
+
+    const handleSubmit = useCallback(async (data: object) => {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome Obrigatorio'),
+                email: Yup.string()
+                    .required('E-mail obrigatorio')
+                    .email('Digite um e-mail valido'),
+                password: Yup.string().min(
+                    6,
+                    ' Senha obrigatoria, minimo 6 caracteres',
+                ),
+            });
+
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
     return (
         <Container>
             <Background />
             <Content>
                 <img src={logoImg} alt="GoBarber" />
 
-                <Form onSubmit={handleSubmit}>
+                <Form ref={formRef} onSubmit={handleSubmit}>
                     <h1>Registre-se</h1>
 
                     <Input icon={FiUser} name="name" placeholder="Nome" />
